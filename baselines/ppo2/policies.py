ed5124e81e6300d50e88446734a6371422f6131f
scutzck033 <404762007@qq.com>
2018-08-15 10:31:23 +0800

    Update policies.py

--- baselines/ppo2/policies.py
+++ baselines/ppo2/policies.py
@@ -291,22 +291,38 @@ class CnnVecSplitPolicy(object):
 
     def __init__(self, sess, ob_space, obs_dict,ac_space, nbatch, nsteps, reuse=False, **conv_kwargs): #pylint: disable=W0613
         self.pdtype = make_pdtype(ac_space)
+        # print(ob_space)
+        # print(type(ob_space))
         ob1_space_size_per_frame=obs_dict['img_obs_shape'][0]*obs_dict['img_obs_shape'][1]*obs_dict['img_obs_shape'][2]
-        ob2_space_size_per_frame = obs_dict['vec_obs_shape']
+        ob2_space_size_per_frame = obs_dict['vec_obs_shape'][0]
+        len_per_frame = ob1_space_size_per_frame + ob2_space_size_per_frame
+
 
         X, processed_x = observation_input(ob_space, nbatch)
         print(np.shape(processed_x))
+
+        ob1_tmp_list = []
+        ob2_tmp_list = []
         with tf.variable_scope("model", reuse=reuse):
-            img_obs_begin = [0,0]
-            img_obs_size = [-1,ob1_space_size_per_frame]
-            vec_obs_begin = [0,ob1_space_size_per_frame]
-            vec_obs_size = [-1,ob2_space_size_per_frame]
-            img_vec_like = tf.slice(processed_x,img_obs_begin,img_obs_size)
-            vec_input = tf.slice(processed_x,vec_obs_begin,vec_obs_size)
-            img_input = tf.reshape(img_vec_like,(nbatch,obs_dict['img_obs_shape'][0],obs_dict['img_obs_shape'][1],
-                                                 obs_dict['img_obs_shape'][2]))
-            print("vec_input",vec_input)
-            print("img_input",img_input)
+            for i in range(obs_dict['num_frame_stack']):
+                curr_img_obs_begin = [0, 0 + i*len_per_frame]
+                curr_img_obs_size = [-1, ob1_space_size_per_frame]
+                curr_vec_obs_begin = [0, ob1_space_size_per_frame + i*len_per_frame]
+                curr_vec_obs_size = [-1, ob2_space_size_per_frame]
+                curr_img_vec_like = tf.slice(processed_x, curr_img_obs_begin, curr_img_obs_size)
+                curr_img = tf.reshape(curr_img_vec_like,
+                                       (nbatch, obs_dict['img_obs_shape'][0], obs_dict['img_obs_shape'][1],
+                                        obs_dict['img_obs_shape'][2]))
+                curr_vec = tf.slice(processed_x, curr_vec_obs_begin, curr_vec_obs_size)
+                ob1_tmp_list.append(curr_img)
+                ob2_tmp_list.append(curr_vec)
+            print(np.shape(ob1_tmp_list[0]))
+            print(type(ob1_tmp_list[0]))
+            img_input = tf.concat([e for e in ob1_tmp_list],axis=3)
+            vec_input = tf.concat([e for e in ob2_tmp_list],axis=1)
+            # print("img obs ******",img_input.shape)
+            # print("vec obs ******",vec_input.shape)
+
             # img feature extractor
             img_h = dqn_cnn(img_input, **conv_kwargs)
 
